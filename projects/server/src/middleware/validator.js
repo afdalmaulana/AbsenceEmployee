efebdcead79b7ba3c302cdaea34727dba0934a02
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2023-08-15 01:04:58 +0800

    absense employee done

--- projects/server/src/middleware/validator.js
+++ projects/server/src/middleware/validator.js
@@ -7,6 +7,33 @@ const loginValidator = [
     .matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/)
     .withMessage("Password must contain 8 character, one uppercase, one number, and one special case character"),
 ]
+
+const registValidatorEmployee = [
+    body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format"),
+  body("roleId").notEmpty().withMessage("Role is required"),
+  body("baseSalary")
+    .notEmpty()
+    .withMessage("Base salary is required")
+    .matches(/(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$/)
+    .withMessage("Must contains only digits"),
+  body("daySalary")
+    .notEmpty()
+    .withMessage("Day salary is required")
+    .matches(/(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$/)
+    .withMessage("Must contains only digits"),
+];
+const formValidatorEmployee = [
+    body("password").notEmpty().withMessage("Password cannot be empty")
+    .matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/)
+    .withMessage("Password must contain 8 character, one uppercase, one number, and one special case character"),
+    body("fullName").notEmpty().withMessage("Fullname is required"),
+    body("username").notEmpty().withMessage("Username is required"),
+    body("birthday").notEmpty().withMessage("Birthday is required"),
+]
 const validateRegist = (req, res, next) => {
     const errors = validationResult(req);
   //   validationResult memiliki method isEmpty untuk mengembalikan nilai true/false
@@ -17,5 +44,5 @@ const validateRegist = (req, res, next) => {
   };
 
   module.exports = {
-    validateRegist, loginValidator
+    validateRegist, loginValidator, formValidatorEmployee, registValidatorEmployee
   }
